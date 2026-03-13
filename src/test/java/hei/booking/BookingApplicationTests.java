5043cb1ccd9dac424b881c88232a21e511bb41c6
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-03-13 09:37:28 +0300

    init

---
+++ src/test/java/hei/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
